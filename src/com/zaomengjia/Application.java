2378ef2f8783aeba1b5bea9bb63918293584929b
orangeboy <chenenhan@qq.com>
2022-04-09 07:03:24 +0800

    update:添加了springboot admin的CI/CD

---
+++ src/com/zaomengjia/Application.java
@@ -0,0 +1,12 @@
+package src.com.zaomengjia;
+
+/**
+ * @author orangeboyChen
+ * @version 1.0
+ * @date 2022/4/9 03:13
+ */
+public class Application {
+    public static void main(String[] args) {
+
+    }
+}
