9a88bb76a1096bf2f805d1d4ba86c1d23848dedf
ICharlotteI <zcharlotteyq@163.com>
2022-04-10 11:07:23 +0800

    添加小程序工具类

--- seckill/utils/util.js
+++ seckill/utils/util.js
@@ -1,3 +1,5 @@
+const validate = require('./validate.js')
+
 const formatTime = date => {
   const year = date.getFullYear()
   const month = date.getMonth() + 1
@@ -6,14 +8,27 @@ const formatTime = date => {
   const minute = date.getMinutes()
   const second = date.getSeconds()
 
-  return `${[year, month, day].map(formatNumber).join('/')} ${[hour, minute, second].map(formatNumber).join(':')}`
+  return [year, month, day].map(formatNumber).join('/') + ' ' + [hour, minute, second].map(formatNumber).join(':')
 }
 
 const formatNumber = n => {
   n = n.toString()
-  return n[1] ? n : `0${n}`
+  return n[1] ? n : '0' + n
+}
+
+//空值过滤器
+const filterForm = (form) => {
+  let obj = {};
+  Object.keys(form).forEach(ele => {
+    if (!validate.validateNull(form[ele])) {
+      obj[ele] = form[ele]
+    }
+  });
+  return obj;
 }
 
 module.exports = {
-  formatTime
+  formatTime: formatTime,
+  formatNumber: formatNumber,
+  filterForm: filterForm
 }
