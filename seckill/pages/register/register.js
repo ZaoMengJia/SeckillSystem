7bc28a194991ea9aea10a723535d1d0274ed0ca1
ICharlotteI <zcharlotteyq@163.com>
2022-04-12 00:54:35 +0800

    完成注册页面

--- seckill/pages/register/register.js
+++ seckill/pages/register/register.js
@@ -1,43 +1,119 @@
 // pages/register/register.js
+import {saveInfo} from "../../utils/api";
+import {validateIdCard} from "../../utils/validate";
+import Toast from "../../miniprogram_npm/@vant/weapp/toast/toast";
+
+const app = getApp()
+
 Page({
 
-  data: {
-    avatarUrl: null,
-    nickName: null,
-    gender: 0,
-    idCard: null,
-    birthday: new Date().getTime(),
-    realName: null,
-    minDate: new Date(1950,1,1).getTime(),
-    maxDate: new Date().getTime(),
-  },
-  onredirectIndex(){
-    wx.redirectTo({
-      url: '../index/index',
-    })
-  },
-  onLoad: function (options) {
-    this.setData({
-      avatarUrl: wx.getStorageSync("userInfo").avatarUrl,
-      nickName: wx.getStorageSync("userInfo").nickName
-    })
-  },
+    data: {
+        avatarUrl: null,
+        nickName: null,
+        gender: "male",
+        idCard: null,
+        birthday: new Date().getTime(),
+        realName: null,
+        cellTitle: "选择出生日期",
+        minDate: new Date(1950, 1, 1).getTime(),
+        maxDate: new Date().getTime(),
+        formatter(type, value) {
+            if (type === 'year') {
+                return `${value}年`;
+            }
+            if (type === 'month') {
+                return `${value}月`;
+            }
+            return value;
+        },
+        show: false
+    },
+    onRedirectIndex() {
+        wx.redirectTo({
+            url: '../index/index',
+        })
+    },
+    onLoad: function (options) {
+        this.setData({
+            avatarUrl: wx.getStorageSync("userInfo").avatarUrl,
+            nickName: wx.getStorageSync("userInfo").nickName
+        })
+    },
 
+    //日期选择器
+    formatDate(date) {
+        date = new Date(date);
+        const YY = date.getFullYear() + '年';
+        const MM = (date.getMonth() + 1 < 10 ? '0' + (date.getMonth() + 1) : date.getMonth() + 1) + '月';
+        const DD = (date.getDate() < 10 ? '0' + (date.getDate()) : date.getDate()) + '日';
+        return YY + MM + DD;
+    },
+    showPopup() {
+        this.setData({
+            show: true,
+            cellTitle: "选择出生日期"
+        });
+    },
+    onClose() {
+        this.setData({
+            show: false,
+            cellTitle: this.formatDate(this.data.birthday)
+        });
+    },
 
-  // 表单相关
-  onChangeNickName(){
-
-  },
-  onChangeRealName(){
-
-  },
-  onChangeIdCard(){
-
-  },
-  onChangeGender(){
-
-  },
-  onChangeDate(){
-
-  }
+    // 表单相关
+    onChangeNickName(event) {
+        this.setData({
+            nickName: event.detail.value
+        })
+    },
+    onChangeRealName(event) {
+        this.setData({
+            realName: event.detail.value
+        })
+    },
+    onChangeIdCard(event) {
+        this.setData({
+            idCard: event.detail.value
+        })
+    },
+    onChangeGender(event) {
+        this.setData({
+            gender: event.detail,
+        })
+    },
+    onChangeDate(event) {
+        this.setData({
+            birthday: event.detail,
+        })
+    },
+    onSaveInfo(){
+        const validation = validateIdCard(this.data.idCard)
+        if(!validation.pass) {
+            Toast.fail(validation.msg)
+            return
+        }
+        const data = {
+            userId: app.globalData.userId,
+            body: {
+                "openid": app.globalData.openId,
+                "nickname": this.data.nickName,
+                "gender": this.data.gender === 'male' ? 1 : 0,
+                "avatarUrl": this.data.avatarUrl,
+                "idCard": this.data.idCard,
+                "birthday": this.data.birthday,
+                "realName": this.data.realName
+            }
+        }
+        const header = {
+            'Authorization': app.globalData.token
+        }
+        saveInfo(data, header).then(res => {
+            console.log(res)
+        }).then(res =>{
+            wx.redirectTo({
+                url: '../product_list/product_list'
+            })
+        })
+    }
 })
--- seckill/utils/validate.js
+++ seckill/utils/validate.js
@@ -89,84 +89,49 @@ export function validateEmail(email) {
 /**
  * 判断身份证号码
  */
-export function cardId(code) {
-    let list = []
-    let result = true
-    let msg = ''
-    const city = {
-        11: '北京',
-        12: '天津',
-        13: '河北',
-        14: '山西',
-        15: '内蒙古',
-        21: '辽宁',
-        22: '吉林',
-        23: '黑龙江 ',
-        31: '上海',
-        32: '江苏',
-        33: '浙江',
-        34: '安徽',
-        35: '福建',
-        36: '江西',
-        37: '山东',
-        41: '河南',
-        42: '湖北 ',
-        43: '湖南',
-        44: '广东',
-        45: '广西',
-        46: '海南',
-        50: '重庆',
-        51: '四川',
-        52: '贵州',
-        53: '云南',
-        54: '西藏 ',
-        61: '陕西',
-        62: '甘肃',
-        63: '青海',
-        64: '宁夏',
-        65: '新疆',
-        71: '台湾',
-        81: '香港',
-        82: '澳门',
-        91: '国外 '
+export function validateIdCard(code) {
+    var city={11:"北京",12:"天津",13:"河北",14:"山西",15:"内蒙古",21:"辽宁",22:"吉林",23:"黑龙江 ",31:"上海",32:"江苏",33:"浙江",34:"安徽",35:"福建",36:"江西",37:"山东",41:"河南",42:"湖北 ",43:"湖南",44:"广东",45:"广西",46:"海南",50:"重庆",51:"四川",52:"贵州",53:"云南",54:"西藏 ",61:"陕西",62:"甘肃",63:"青海",64:"宁夏",65:"新疆",71:"台湾",81:"香港",82:"澳门",91:"国外 "};
+    var row={
+        'pass':true,
+        'msg':'验证成功'
     };
-    if (!validateNull(code)) {
-        if (code.length == 18) {
-            if (!code || !/(^\d{18}$)|(^\d{17}(\d|X|x)$)/.test(code)) {
-                msg = '证件号码格式错误'
-            } else if (!city[code.substr(0, 2)]) {
-                msg = '地址编码错误'
-            } else {
-                // 18位身份证需要验证最后一位校验位
-                code = code.split('')
-                // ∑(ai×Wi)(mod 11)
-                // 加权因子
-                const factor = [7, 9, 10, 5, 8, 4, 2, 1, 6, 3, 7, 9, 10, 5, 8, 4, 2];
-                // 校验位
-                const parity = [1, 0, 'X', 9, 8, 7, 6, 5, 4, 3, 2, 'x'];
-                let sum = 0;
-                let ai = 0;
-                let wi = 0;
-                for (let i = 0; i < 17; i++) {
-                    ai = code[i]
-                    wi = factor[i]
-                    sum += ai * wi
-                }
-                if (parity[sum % 11] != code[17]) {
-                    msg = '证件号码校验位错误'
-                } else {
-                    result = false
-                }
+    if(!code || !/^\d{6}(18|19|20)?\d{2}(0[1-9]|1[012])(0[1-9]|[12]\d|3[01])\d{3}(\d|[xX])$/.test(code)){
+        row={
+            'pass':false,
+            'msg':'身份证号格式错误'
+        };
+    }else if(!city[code.substr(0,2)]){
+        row={
+            'pass':false,
+            'msg':'身份证号地址编码错误'
+        };
+    }else{
+        //18位身份证需要验证最后一位校验位
+        if(code.length == 18){
+            code = code.split('');
+            //∑(ai×Wi)(mod 11)
+            //加权因子
+            var factor = [ 7, 9, 10, 5, 8, 4, 2, 1, 6, 3, 7, 9, 10, 5, 8, 4, 2 ];
+            //校验位
+            var parity = [ 1, 0, 'X', 9, 8, 7, 6, 5, 4, 3, 2 ];
+            var sum = 0;
+            var ai = 0;
+            var wi = 0;
+            for (var i = 0; i < 17; i++)
+            {
+                ai = code[i];
+                wi = factor[i];
+                sum += ai * wi;
             }
-        } else {
-            msg = '证件号码长度不为18位'
+            if(parity[sum % 11] != code[17].toUpperCase()){
+                row={
+                    'pass':false,
+                    'msg':'身份证号校验位错误'
+                };
+            }
         }
-    } else {
-        msg = '证件号码不能为空'
     }
-    list.push(result)
-    list.push(msg)
-    return list
+    return row;
 }
 
 /**
@@ -255,5 +220,6 @@ export function validateNull(val) {
 }
 
 module.exports = {
-    validateNull: validateNull
+    validateNull: validateNull,
+    validateIdCard: validateIdCard
 }
