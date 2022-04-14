96943ce5d0179ccd959bfcfc16fb31f82677b670
ICharlotteI <zcharlotteyq@163.com>
2022-04-14 20:22:56 +0800

    修改部分逻辑

--- seckill/app.js
+++ seckill/app.js
@@ -1,4 +1,4 @@
-    import api, {BASE_PATH, login} from "./utils/api";
+import {login} from "./utils/api";
 
 App({
     onLaunch() {
@@ -11,22 +11,6 @@ App({
                 let custom = wx.getMenuButtonBoundingClientRect();
                 this.globalData.Custom = custom;
                 this.globalData.CustomBar = custom.bottom + custom.top - e.statusBarHeight;
-            }
-        })
-        // 登录
-        wx.login({
-            success: res => {
-                //发送 res.code 到后台换取 openId, token
-                login({
-                    code: res.code
-                }).then(res => {
-                    this.globalData.token = res.data.token
-                    this.globalData.auth = this.globalData.token.replace("Bearer ", "")
-                    this.globalData.userId = res.data.userId
-                    this.globalData.openId = res.data.openId
-                }).then(res => {
-                    console.log(this.globalData)
-                })
             }
         })
     }
